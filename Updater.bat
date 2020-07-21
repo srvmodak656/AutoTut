2f1f2828a56c6381efb40202dcff07d7cc850e1a
Sourav Modak <35265849+srvmodak656@users.noreply.github.com>
2020-07-21 10:39:29 +0530

    Delete Updater.bat

--- Updater.bat
+++
@@ -1,9 +0,0 @@
-timeout 5
-taskkill /IM "AutoTut.exe"
-wmic Path win32_process Where "CommandLine Like '%AutoTut.jar%'" Call Terminate
-git stash
-git pull
-java -jar Updater.jar 3
-timeout 5
-AutoTut.exe
-exit
